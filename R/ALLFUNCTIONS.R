bf4a01a5848ca07f57e77f1d6e563cfe66679e77
Will Schulz <wschulz@princeton.edu>
2021-07-06 11:40:42 -0400

    fix indexer

--- R/ALLFUNCTIONS.R
+++ R/ALLFUNCTIONS.R
@@ -2141,10 +2141,10 @@ singleNodeIndexer <- function(twitter_user_id, thismetadata){return(thismetadata
 
 nodeIndexer <- function(twitter_user_id, metadata){
   if (length(twitter_user_id)==1){
-    return(singeNodeIndexer(twitter_user_id,metadata))
+    return(singeNodeIndexer(twitter_user_id,thismetadata=metadata))
   }
   if (length(twitter_user_id)>1){
-    return(sapply(twitter_user_id,singleNodeIndexer))
+    return(sapply(twitter_user_id,thismetadata=singleNodeIndexer))
   }
 }
 
