55aad3eece1417fbd547d6ef2adba19a19f80268
Will Schulz <wschulz@princeton.edu>
2021-07-07 19:40:34 -0400

    assignment sampling script down to 1 minute

--- inst/scripts/assignment_sampling.R
+++ inst/scripts/assignment_sampling.R
@@ -4,4 +4,4 @@ library(tricordR)
 tokens <- prepTokens("ws_tw", 1:9)
 
 Sys.sleep(round(runif(1, 60, 1800)))
-sampleEnglishTweeters5k(study_name = "spirals_assignment_sampling", token = tokens[[1]], minutes = 5)
+sampleEnglishTweeters5k(study_name = "spirals_assignment_sampling", token = tokens[[1]], minutes = 1)
