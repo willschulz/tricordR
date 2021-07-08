1753d914dda4d8e4b9074173716476e5a7d54756
Will Schulz <wschulz@princeton.edu>
2021-07-08 17:18:27 -0400

    assignment sampling script moved to assignment sampling repo

--- inst/scripts/assignment_sampling.R
+++
@@ -1,7 +0,0 @@
-# assignment sampler
-
-library(tricordR)
-tokens <- prepTokens("ws_tw", 1:9)
-
-Sys.sleep(round(runif(1, 60, 1800)))
-sampleEnglishTweeters5k50kRatioHalf(study_name = "spirals_assignment_sampling", token = tokens[[1]], minutes = 5)
