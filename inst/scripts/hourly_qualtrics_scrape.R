560e08f37721d6cfb3c4047bb8f3df0b0115305b
Will Schulz <wschulz@princeton.edu>
2021-07-21 14:03:45 -0400

    qualtrics scraping function updated to use async matching function

--- inst/scripts/hourly_qualtrics_scrape.R
+++ inst/scripts/hourly_qualtrics_scrape.R
@@ -24,7 +24,7 @@ for (i in 1:length(panel_directories)) {
   this_study_name <- names[1]
   this_panel_name <- names[2]
   scrapeQualtrics(study_name = this_study_name, panel_name = this_panel_name, match_by = "follow3",
-                  assignment_dir = NULL, max_treat_followers = 60000,
+                  assignment_panel = NULL, max_treat_followers = 60000,
                   treatment_tokens = tokens, participant_tokens = tokens)
 }
 
